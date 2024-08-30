f38f19ee0614f3ef7799524cfa8f849e7bfc29cd
AmeraElsa3id <ameraelsa3id@gmail.com>
2024-08-30 17:59:46 +0300

    modifing go back button

--- src/Components/SubCategories/SubCategories.jsx
+++ src/Components/SubCategories/SubCategories.jsx
@@ -6,7 +6,7 @@ export default function SubCategories() {
     let {name}=useParams();
     console.log(name)
   return (<>
-    <div className='mt-24 ms-10 bg-teal-600 text-white w-[15%] p-3 rounded-md hover:bg-teal-800 transition-all duration-200'><Link to={'/category'} className='flex justify-center items-center gap-2'> <i className='fa fa-arrow-left'></i> go back to category list</Link></div>
+    <div className='mt-24 ms-10 bg-teal-600 text-white w-fit p-3 rounded-md hover:bg-teal-800 transition-all duration-200'><Link to={'/category'} className='flex justify-center items-center gap-2'> <i className='fa fa-arrow-left'></i> go back to category list</Link></div>
     <Category categoryName={name}></Category>
   </>
   )
